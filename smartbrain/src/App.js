b887518c2d50504653348246d2c51912eb1bbc96
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-01-19 15:51:35 +0000

    Navigation and Logo

---
+++ smartbrain/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Navigation from './Components/Navigation/Navigation';
+import Logo from './Components/Logo/Logo';
+// import 'tachyons';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      {/* <ImageLinkForm />
+      <FaceRecognition /> */}
+    </div>
+  );
+}
+
+export default App;
