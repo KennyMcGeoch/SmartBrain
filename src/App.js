bb9b05b7de61eb4012954c17e0105f7172b70cb1
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-01-20 13:42:05 +0000

    Added particles to page

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Rank from './Components/Rank/Rank';
+import Particles from "react-tsparticles";
 
 
 
 function App() {
+  
   return (
     <div className="App">
+      <Particles options={particleOpts}/>
       <Navigation />
       <Logo />
       <Rank />
@@ -17,6 +20,88 @@ function App() {
       {/* <FaceRecognition /> */}
     </div>
   );
+}
+
+const particleOpts = {
+  fullScreen: {
+    enable: true,
+    zIndex: -1
+  },
+  background: {
+    image:
+      "url(https://img4.goodfon.com/original/1920x1080/d/b1/abstract-dark-blue-polygonal-background-abstraktsiia-geometr.jpg?d=1)"
+  },
+  backgroundMask: {
+    enable: true
+  },
+  fpsLimit: 60,
+  emitters: {
+    direction: "random",
+    size: {
+      width: 100,
+      height: 100
+    },
+    position: {
+      x: 50,
+      y: 50
+    },
+    rate: {
+      delay: 0.25,
+      quantity: 2
+    }
+  },
+  particles: {
+    number: {
+      value: 0
+    },
+    color: {
+      value: ["#fff"]
+    },
+    shape: {
+      type: "circle"
+    },
+    opacity: {
+      value: 0.5
+    },
+    size: {
+      value: 200,
+      anim: {
+        enable: true,
+        speed: 50,
+        size_min: 2,
+        sync: true,
+        startValue: "min",
+        destroy: "max"
+      }
+    },
+    move: {
+      enable: true,
+      speed: 5,
+      direction: "none",
+      random: false,
+      straight: false,
+      outModes: {
+        default: "destroy"
+      },
+      attract: {
+        enable: true,
+        distance: 250,
+        rotateX: 600,
+        rotateY: 1200
+      }
+    },
+    stroke: {
+      width: 10,
+      opacity: 1
+    }
+  },
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      resize: true
+    }
+  },
+  detectRetina: true
 }
 
 export default App;
--- src/Components/Rank/Rank.jsx
+++ src/Components/Rank/Rank.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Rank = () => {
     return(
         <div >
-            <div className='white f3 centre'>
+            <div className='black f3 centre'>
                 {'Kenny, your current rank is...'}
             </div>
-            <div className='white f1 centre'>
+            <div className='black f1 centre'>
                 {'#5'}
             </div>
         </div>
