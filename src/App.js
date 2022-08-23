c15b611468958c1485a1ee7a78b97309d94e3c2c
KennyMcGeoch <87940843+KennyMcGeoch@users.noreply.github.com>
2022-08-23 14:54:33 +0100

    Changed endpoints from localhost:3000 toHeroku

--- src/App.js
+++ src/App.js
@@ -70,7 +70,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://lit-meadow-09763.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -81,7 +81,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://lit-meadow-09763.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
